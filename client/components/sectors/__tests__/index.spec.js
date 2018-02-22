54aef4834c22a4b3feea3c7c1ff67d864d5dbdf7
Ionut Cirja <ionut.cirja@gmail.com>
2018-02-22 13:41:20 +0000

    refactoring

--- client/components/sectors/__tests__/index.spec.js
+++ client/components/sectors/__tests__/index.spec.js
@@ -17,9 +17,10 @@ describe('Sectors component', () => {
       const wrapper = shallow(<Component {...renderProps} />);
       const list = wrapper.find('li');
       const listLength = list.length;
+      let listItem;
       expect(listLength).toEqual(renderProps.sectorsList.length);
       for (let i = 0; i < listLength; i += 1) {
-        const listItem = list.at(i);
+        listItem = list.at(i);
         expect(listItem.text()).toEqual(renderProps.sectorsList[i]);
         expect(listItem.props().className).toContain(renderProps.sectorsList[i]);
       }
