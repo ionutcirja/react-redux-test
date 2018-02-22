c5731e8a58969622462677029304e49a0290e8a8
Ionut Cirja <ionut.cirja@gmail.com>
2018-02-22 12:04:31 +0000

    create application main entry file

--- client/index.js
+++ client/index.js
@@ -1,1 +1,22 @@
-console.log('setup');
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import initStore from './store';
+import App from './components/app';
+import './styles/index.scss';
+
+const SECTORS_LIST = [
+  'automobile',
+  'agriculture',
+  'agrifood',
+  'commodities',
+];
+
+const store = initStore();
+
+render(
+  <Provider store={store}>
+    <App sectors={SECTORS_LIST} />
+  </Provider>,
+  document.getElementById('root'),
+);
